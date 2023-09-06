2a1c165b3c8919f089deee06b18d73f9790673fb
Zahid Hussain <zahid.hussain@zonicsolutions.com>
2023-09-06 15:17:06 +0500

    Header section and Login authentication

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,14 @@ import { FaTruckMoving } from 'react-icons/fa';
 import { AiOutlineHeart } from 'react-icons/ai';
 import { BsBagCheck } from 'react-icons/bs';
 import { AiOutlineUser } from 'react-icons/ai';
+import { CiLogin } from 'react-icons/ci';
+import { CiLogout } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
 import './nav.css'
 
 const Nav = () => {
+    const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
     return (
         <>
             <div className="free">
@@ -26,16 +30,53 @@ const Nav = () => {
                         <button>Search</button>
                     </div>
                     <div className='icon'>
-                        <div className='account'>
+                    {
+                        isAuthenticated && 
+                        (
+                            <div className='account'>
                             <div className='user-icon'>
                                 <AiOutlineUser />
                             </div>
-                            <p>Hello, User</p>
+                            <p>Hello, {user.name}</p>
                         </div>
+                        )
+                    }
+                        
                         <div className='second-icon'>
                             <Link to="/" className='link'><AiOutlineHeart /></Link>
                             <Link to="/cart" className='link'><BsBagCheck /></Link>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className='header'>
+                <div className='container'>
+                    <div className='nav'>
+                        <ul>
+                            <li>
+                                <Link to='/' className='link'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/product' className='link'>Product</Link>
+                            </li>
+                            <li>
+                                <Link to='/about' className='link'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact' className='link'>Conact</Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className='auth'>
+                    {
+                        isAuthenticated ?
+                        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}><CiLogout /></button>
+                        :
+                        <button onClick={()=> loginWithRedirect()}><CiLogin /></button>
+                    }
+                        
+                        
                     </div>
                 </div>
             </div>
